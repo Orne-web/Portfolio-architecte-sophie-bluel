d37660a9307a56d3e98dad9653afffb421854c88
ornela <akofa.mivedor@gmail.com>
2026-07-15 16:35:13 +0200

    Creation page login et connexion formulaire

---
+++ FrontEnd/Scripts/login.js
@@ -0,0 +1,31 @@
+console.log("login.js fonctionne");
+const form = document.querySelector("#login-form");
+
+form.addEventListener("submit", async (event) => {
+
+    event.preventDefault();
+    console.log("formulaire envoyé");
+
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+
+    const response = await fetch("http://localhost:5678/api/users/login", {
+
+        method: "POST",
+    
+        headers: {
+            "Content-Type": "application/json"
+        },
+    
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
+    
+    });
+    
+    const data = await response.json();
+    
+    console.log(data);
+
+});
