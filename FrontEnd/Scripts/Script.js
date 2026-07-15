4855e9db464abb2232396a9d87af4d34ae63605a
ornela <akofa.mivedor@gmail.com>
2026-07-15 13:57:39 +0200

    Affichage dynamique des projets depuis l API

---
+++ FrontEnd/Scripts/Script.js
@@ -0,0 +1,26 @@
+async function getWorks() {
+    const response = await fetch("http://localhost:5678/api/works");
+    const works = await response.json();
+
+    const gallery = document.querySelector(".gallery");
+    gallery.innerHTML = "";
+
+    works.forEach(work => {
+    
+
+    const figure = document.createElement("figure");
+
+    const image = document.createElement("img");
+    image.src = work.imageUrl;
+    image.alt = work.title;
+
+    const caption = document.createElement("figcaption");
+    caption.textContent = work.title;
+
+    figure.appendChild(image);
+    figure.appendChild(caption);
+
+    gallery.appendChild(figure);
+}
+
+getWorks();
